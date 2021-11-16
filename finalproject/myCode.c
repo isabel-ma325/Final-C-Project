06d9c2a45d14b2c4395ba310f73f86a337eca6ec
YOUR_FIRST_NAME YOUR_LAST_NAME <YOUR@EMAIL_ADDRESS>
2021-11-16 19:47:56 +0000

    draft

--- finalproject/myCode.c
+++ finalproject/myCode.c
@@ -1,5 +1,38 @@
 #include <stdio.h>
-
+#define MAXTEST 5
+#define MAXTIME 30
+/* structure to store info for each exam*/
+struct examinfo {
+	int examtime;
+	int examdiff;
+    float signif;
+};
+/* structure to store how much free time there is */
+struct dailytime {
+	int freetime;
+};
 int main(void) {
-	printf("Hello, World!\n");
+	int examnum;
+	struct examinfo arr_examinfo[MAXTEST]; //making an array of structures
+    struct dailytime arr_dailytime[7];
+
+	printf("How many exams do you have? ");
+	scanf("%d", &examnum);
+	if (examnum > MAXTEST){ //making sure number of exams is under six
+		printf("Please enter a value under 6: ");
+		scanf("%d", &examnum);
+	}
+	for (int i=1; i<= examnum; i++){ //storing information of each exam
+		printf("How hard is exam %d on a scale of 1 to 5? ", i);
+		scanf("%d", &arr_examinfo[i].examdiff);
+		printf("How much time do you have to study for exam %d? ", i);
+		scanf("%d", &arr_examinfo[i].examtime);
+	}
+    for (int j=1; j <= 7; j++){ //storing information for free time
+        printf("How much free time do you have on day %d of the week? ", j);
+        scanf("%d", &arr_dailytime[j].freetime);
 }
+
+
+}
+
